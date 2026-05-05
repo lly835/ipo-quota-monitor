9fe800f2cb37316e9a08813bda97f004dd8162a8
lly835 <lly835@163.com>
2026-05-05 13:59:57 +0800

    remove api/index.py catching all routes

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(302)
-        self.send_header('Location', '/')
-        self.end_headers()
